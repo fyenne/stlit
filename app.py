46093a4e2589465ba97b635401a4e7121c1c8e43
fyenne <fyenne@hotmail.com>
2022-06-16 15:57:14 +0800

    commit

--- app.py
+++ app.py
@@ -76,26 +76,21 @@ class load_data:
     
 st.title('正德佷准')
 st.markdown('--- \n > **人工智能和量子物理结合下的优化版梅花易数**\n ---')
-# ---- 
-
-# # l2=[]
-# #  将纵向每个字符当作 y 坐标的刻度
-# for y in range(15,-15,-1):
-#     l3 = []
-#     #  将横向每个字符当作 x 坐标的刻度
-#     for x in range(-30,30):
-#         # 如果 x,y 点在心形内,则将一个字符加入到行,否则加入空字符
-#         l3.append((' I love U'[(x-y)%9]if((x*0.05)**2+(y*0.1)**2-1)**3-(x*0.05)**2*(y*0.1)**3<=0 else' '))
-#     l2.append(''.join(l3))
-# l1 ='\n'.join(l2) 
-
-    
-st.text('hahha')
+add_selectbox = st.sidebar.selectbox(
+    "In which aspects would you like to know your future?",
+    ("undefined", "money", "love")
+)
 # ----
-selected_target = st.selectbox('测什么?', options = ['选择', '情路', '仕途'])
-if selected_target == '选择':
+# selected_target = st.selectbox('测什么?', options = ['选择', '情路', '仕途'])
+
+if add_selectbox == 'undefined':
     print('')
-    st.plotly_chart(load_data.plt1())
+    
+    with st.container():
+        st.write("This is inside the container")
+        st.plotly_chart(load_data.plt1())
+    
+    st.write("This is outside the container")
 # -----
 else:
     '今日是农历: %s \
