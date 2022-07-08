05e87df52971bc086832d6d951972b3859233c34
fyenne <fyenne@hotmail.com>
2022-07-08 19:11:01 +0800

    co

--- mail_sender.py
+++ mail_sender.py
@@ -3,7 +3,8 @@ import re
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText 
-
+from email.mime.base import MIMEBase
+from email import encoders
 
 
 class config():
@@ -12,20 +13,40 @@ class config():
         self.mail = 'fyenneyenn@hotmail.com'
         self.pw = 'NOmoreuse7-'
     
-    def sendEmailwithFile(self, account, server, content, receivers):
+    def sendEmailwithFile(self, account, server, content, payload, receivers):
         """
         对每一行pd df_row 的循环. 
         """
+        msg = MIMEMultipart()
+        subject = 'message__' + str(datetime.now()) 
+        msg['Subject'] = subject
+        
+        # ============================================================================ #
+        #                                      txt                                     #
+        # ============================================================================ #
         text = '''<p style="font-family:等线;font-size:10.5pt">memo: 
             <mark><b>mooo</b></mark><br />
                     '''
         text = text + content
-        subject = 'message__' + str(datetime.now()) 
-        
-        msg = MIMEMultipart()
-        msg['Subject'] = subject
         msg.attach(MIMEText(text, 'html', 'utf-8'))
+        
+        # ============================================================================ #
+        #                                      file                                    #
+        # ============================================================================ #
+        # with open(attachment, 'rb') as fp:
+        # img = MIMEImage(file)
+        # img.add_header('Content-ID', '<{}>'.format('a pic'))
+        # msg.attach(img)
+        output_name1 = 'name'
+        part1 = MIMEBase('application', "octet-stream")
+        part1.set_payload(payload)
+        encoders.encode_base64(part1)
+        part1.add_header('Content-Disposition', 'attachment', filename=('utf-8', '', output_name1))
+        msg.attach(part1)
 
+        # ============================================================================ #
+        #                                    config                                    #
+        # ============================================================================ #
         msg['From'] = account
         receivers = receivers
         msg['TO'] = receivers
@@ -34,12 +55,12 @@ class config():
         server.sendmail(account, receivers, text)
         
         
-    def run_(self, content , receivers):        
+    def run_(self, content , payload, receivers):        
         self.server.starttls()
         self.server.login(self.mail, self.pw)
         print('connecting...')
         try:
-            self.sendEmailwithFile(self.mail, server = self.server, content = content, receivers=receivers)
+            self.sendEmailwithFile(self.mail, server = self.server, content = content, payload=payload, receivers=receivers)
             print("SUCCESS")
         except Exception as e:
             raise ValueError(e)
--- pages/🎉p3.py
+++ pages/🎉p3.py
@@ -1,5 +1,7 @@
 
 # Contents of ~/my_app/pages/page_3.py
+from email.policy import default
+from aiohttp import Payload
 from blinker import receiver_connected
 import streamlit as st
 from mail_sender import config
@@ -9,18 +11,29 @@ st.sidebar.markdown("# Page 3 🎉")
 
 
 
-receiver = st.selectbox("to_whom", options = ["fyenne@hotmail.com"])
-content  = st.text_input('content')
-bt = st.button('send')
+# add_plus = st.text_input('add a mail address')
+# if add_plus is not None:
+#     select_list = ["fyenne@hotmail.com"].append(add_plus)
+#     st.write(select_list)
+# else:
+select_list = ["fyenne@hotmail.com"]
 
+receiver = st.selectbox("to_whom", options = select_list)
+content  = st.text_input('content')
+file = st.file_uploader(label="file uploader", 
+                        accept_multiple_files = True)
+
+
+
+bt = st.button('send')
 try:
     receiver = re.findall('\w+\@\w+\.\w+', receiver)[0]
-    st.write('ok')
+    st.write('email format ok')
 except:
     st.write('wrong')
     pass
 if bt:
     a = config()
     print(receiver)
-    a.run_(str(content), str(receiver))
+    a.run_(content = str(content), payload=file, receivers = str(receiver))
     # a.run_('aa', 'fyenne@hotmail.com')
