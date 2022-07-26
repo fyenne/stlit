093776c7cca5d00003444cf6c1e3e21528860809
fyenne <fyenne@hotmail.com>
2022-07-26 14:24:36 +0800

    commit

--- pages/❄️p2.py
+++ pages/❄️p2.py
@@ -1,7 +1,7 @@
 # Contents of ~/my_app/main_page.py
 import pandas as pd
 import streamlit as st
-import time
+from datetime import datetime
 st.markdown("# Page 2 ❄️")
 st.sidebar.markdown("# Page 2 ❄️")
 
@@ -9,17 +9,32 @@ st.sidebar.markdown("# Page 2 ❄️")
 
 
 
-with st.form("my_form"):
+with st.form("input form"):
     message = st.text_input("user_name")
     slider_val = st.slider("Form slider")
     checkbox_val = st.checkbox("Form checkbox")
-
+    dt = datetime.now()
+    
     # Every form must have a submit button.
     submitted = st.form_submit_button("Submit")
     if submitted:
-        st.write("user" ,message)
-        st.write("slider", slider_val, "checkbox", checkbox_val)
-        pd.DataFrame({'message': message, 'slider_val':slider_val, 'checkbox_val': checkbox_val}, index=range(1)).to_csv('./dataup/backup.csv')        
-    # with st.spinner('Wait for it...'):
+        message = pd.Series([i.strip() for i in message.split(',')])
+        # st.write("user" ,message)
+        # st.write("slider", slider_val, "checkbox", checkbox_val)
+        df = pd.DataFrame(
+            {
+                'message': message,
+                'slider_val':slider_val,
+                'checkbox_val': checkbox_val,
+                'datetime': dt
+            }, 
+            index=range(len(message))).head(20)
+        st.table(df)
+        df.to_csv('./dataup/backup.csv', index = None, encoding = 'utf_8_sig')        
 
+with st.form("query form"):
+    submitted = st.form_submit_button("Submit")
+    if submitted:
+        df = pd.read_csv('./dataup/backup.csv')
+        st.table(df)
 
